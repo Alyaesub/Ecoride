4a440c95b9cdb6620682c44132fb6ce8505fc861
Alyaesub <dadz13104@gmail.com>
2025-04-24 13:01:43 -0700

    finish css register form

--- app/views/pages/register.php
+++ app/views/pages/register.php
@@ -1,28 +1,33 @@
 <?php
 $title = 'Créer votre profile';
 ?>
-<h1>Créer votre profil</h1>
-<form action="" method="post">
-  <label>Pseudo :</label>
-  <input type="text" name="pseudo" required><br>
+<section class="registerMain">
+  <h1>Créer votre profil</h1>
+  <form class="registerForm" action="" method="post">
+    <label>Pseudo :</label>
+    <input type="text" name="pseudo" required><br>
 
-  <label>Nom :</label>
-  <input type="text" name="nom"><br>
+    <label>Nom :</label>
+    <input type="text" name="nom"><br>
 
-  <label>Prénom :</label>
-  <input type="text" name="prenom"><br>
+    <label>Prénom :</label>
+    <input type="text" name="prenom"><br>
 
-  <label>Email :</label>
-  <input type="email" name="email" required><br>
+    <label>Email :</label>
+    <input type="email" name="email" required><br>
 
-  <label>Mot de passe :</label>
-  <input type="password" name="mot_de_passe" required><br>
+    <label>Mot de passe :</label>
+    <input type="password" name="mot_de_passe" required><br>
 
-  <label>Vous êtes :</label>
-  <select name="id_role" required>
-    <option value="1">Chauffeur</option>
-    <option value="2">Passager</option>
-  </select><br>
+    <div class="role-button">
+      <label>Vous êtes :</label>
+      <select name="id_role" required>
+        <option value="1">Chauffeur</option>
+        <option value="2">Passager</option>
+      </select><br>
+    </div>
 
-  <button type="submit">Créer mon profil</button>
-</form>
+
+    <button type="submit">Créer mon profil</button>
+  </form>
+</section>
